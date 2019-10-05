e11aa1e6728be7fb699b80d8d8d3420c9ee8d099
Vicente Albuquerque <7.vincent@gmail.com>
2019-10-05 00:39:17 -0300

    Inserido service axios, animacao no bt plus e lista de repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,79 @@
-import React from "react";
-import { FaUserCircle, FaPlus } from "react-icons/fa";
+import React, { Component } from "react";
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
 
-import { Container, Form, SubmitButton } from "./styles";
+import api from "../../services/api";
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaUserCircle />
-        Fala Brother!
-      </h1>
+import { Container, Form, List, SubmitButton } from "./styles";
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Seu nome..." />
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newRepo: "",
+    repositories: [],
+    loading: false
+  };
+
+  //pegando dados do input
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  //pegando o submit
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    //pegando o nome do repos digitado no form e os repos
+    const { newRepo, repositories } = this.state;
+    const response = await api.get(`repos/${newRepo}`);
+
+    //criando uma variável para armazenar o repos
+    const data = {
+      name: response.data.full_name
+    };
+
+    //modificando o stado da variável que guardas uma lista de repos
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: "",
+      loading: false
+    });
+    //console.log(response.data);
+  };
+
+  render() {
+    const { newRepo, repositories, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithub />
+          Fala Brother!
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Seu nome..."
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -34,7 +34,21 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button`
+/*
+Animação do loading
+*/
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`;
+export const SubmitButton = styled.button.attrs(props => ({
+  type: "submit",
+  disabled: props.loading
+}))`
   background: #87ceeb;
   border: 0;
   padding: 0 15px;
@@ -44,4 +58,42 @@ export const SubmitButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  /* Criando animação para o loading
+  obs: usando operadores ternarioa abaixo, só que não temos o else, somente o if, ai usamod o &&
+   */
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    /*so add o stilo que tem um li antes*/
+    & + li {
+      border-top: 1px solid gray;
+    }
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
 `;
